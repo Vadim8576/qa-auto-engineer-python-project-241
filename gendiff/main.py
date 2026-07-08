fccb992b472128ce5d308fb35a11e50d65cf79e1
Vadim8576 <vr-project@yandex.ru>
2026-07-08 20:51:59 +0300

    parse files

--- gendiff/main.py
+++ gendiff/main.py
@@ -1,5 +1,7 @@
 import argparse
 
+import json
+
 
 def start():   
     parser = argparse.ArgumentParser(
@@ -17,7 +19,16 @@ def start():
     )
 
     args = parser.parse_args()
+    first_file = args.first_file
+    second_file = args.second_file
+    output_format = args.format
     
     print(args)
+    
+    file1 = json.load(open(f'gendiff/files/{first_file}'))
+    file2 = json.load(open(f'gendiff/files/{second_file}'))
+    
+    print(file1)
+    print(file2)
 
     
